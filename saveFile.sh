920b92364c890ebc0dcabec4cc98be09423fa8ce
komo <komo@modeledebian.example.local>
2017-12-06 10:09:28 +0100

    Premier envoi

---
+++ saveFile.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+ZEDATE=`date +%Y-%m-%d_%H-%M-%S`
+
+mkdir -p /srv/testSYNC/INC/${ZEDATE}/
+
+/usr/bin/rsync -av --stats --delete --ignore-errors --backup \
+	--backup-dir=/srv/testSYNC/INC/${ZEDATE}/ \
+	/srv/FreeNas/ \
+	/srv/testSYNC/BACK/
+
